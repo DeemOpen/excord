da99d7c3cfc7fe417d451fbcd5bd07217f19e537
Arjun Surendra <arjun.surendra04@gmail.com>
2016-01-18 00:04:48 -0800

    bug fixed for bulkd edit testcase type

--- src/main/resources/db/migration/V3__ChangeSet1.sql
+++ src/main/resources/db/migration/V3__ChangeSet1.sql
@@ -3,7 +3,7 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 CREATE TABLE IF NOT EXISTS `excord`.`ec_requirement` (
-  `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '',
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(90) NULL,
   `parent_id` BIGINT(20) NULL,
   `priority` VARCHAR(45) NULL,
@@ -12,9 +12,9 @@ CREATE TABLE IF NOT EXISTS `excord`.`ec_requirement` (
   `product` VARCHAR(90) NULL,
   `coverage` TINYINT(1) NOT NULL,
   `story` TEXT NULL,
-  PRIMARY KEY (`id`)  COMMENT '',
-  INDEX `requirement_idx1` (`parent_id` ASC)  COMMENT '',
-  UNIQUE INDEX `requirement_idx2` (`id` ASC, `parent_id` ASC)  COMMENT '',
+  PRIMARY KEY (`id`),
+  INDEX `requirement_idx1` (`parent_id` ASC),
+  UNIQUE INDEX `requirement_idx2` (`id` ASC, `parent_id` ASC),
   CONSTRAINT `requirement_mapping_fk1`
     FOREIGN KEY (`parent_id`)
     REFERENCES `excord`.`ec_requirement` (`id`)
@@ -25,13 +25,13 @@ DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_general_ci;
 
 CREATE TABLE IF NOT EXISTS `excord`.`ec_testcase_requirement_mapping` (
-  `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '',
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
   `requirement_id` BIGINT(20) NOT NULL,
   `testcase_id` BIGINT(20) NOT NULL,
   `review` TINYINT(1) NOT NULL,
-  PRIMARY KEY (`id`)  COMMENT '',
-  INDEX `tc_req_mapping_fk_idx` (`requirement_id` ASC)  COMMENT '',
-  INDEX `tc_req_mapping_fk2_idx` (`testcase_id` ASC)  COMMENT '',
+  PRIMARY KEY (`id`),
+  INDEX `tc_req_mapping_fk_idx` (`requirement_id` ASC),
+  INDEX `tc_req_mapping_fk2_idx` (`testcase_id` ASC),
   CONSTRAINT `tc_req_mapping_fk1`
     FOREIGN KEY (`requirement_id`)
     REFERENCES `excord`.`ec_requirement` (`id`)
